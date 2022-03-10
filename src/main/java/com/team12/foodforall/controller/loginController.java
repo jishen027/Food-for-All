d2a4688137cef2e29d48bf5eb6d57286b030bf14
H Gao <hgao26@sheffield.ac.uk>
2022-03-10 14:57:25 +0000

    [update] update login controller and login form

--- src/main/java/com/team12/foodforall/controller/loginController.java
+++ src/main/java/com/team12/foodforall/controller/loginController.java
@@ -1,11 +1,15 @@
 package com.team12.foodforall.controller;
 
 import com.team12.foodforall.domain.LoginForm;
+import com.team12.foodforall.domain.Response;
+import lombok.val;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 /**
  * @author: Heng Gao
@@ -13,23 +17,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
  **/
 @Controller
 @RequestMapping("/login")
+@Validated
 public class loginController {
 
 
     @GetMapping()
-    public String _login(){
+    public String _login() {
         //TODO:
 
         return "login";
     }
 
     @PostMapping()
-    public String auth(){
-        //TODO:
+    @ResponseBody // 一般返回json
+    public ResponseEntity<Response<String>> auth(@RequestBody LoginForm loginForm) {
+        val form = Optional.ofNullable(loginForm)
+                .orElseThrow(() -> new RuntimeException("body cannot be not"));
 
-        //解析
+        if ("gh".equals(form.getUsername()) && "1233".equals(form.getPassword())) {
+            return ResponseEntity.ok(Response.<String>builder().code(HttpStatus.OK.value()).data("succ").build());
+        }
 
-        //
-        return "redirect:";
+        // TODO：改成统一处理
+//        return ResponseEntity.ok("200"); //spring 会检测并设置成text
+        return ResponseEntity.status(401).body(Response.<String>builder().code(HttpStatus.UNAUTHORIZED.value()).data("invalid passwrod").build());
     }
 }
