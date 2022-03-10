d27595249531b44678682ac3c9930c261b5115d0
Tian Yuhao <ytian47@sheffield.ac.uk>
2022-03-10 19:12:39 +0000

    [merge] merge Feature login to dev

---
+++ src/main/java/com/team12/foodforall/domain/LoginForm.java
@@ -0,0 +1,13 @@
+package com.team12.foodforall.domain;
+
+import lombok.Data;
+
+/**
+ * @author: Heng Gao
+ * @date: 08/03/2022 :17:35
+ **/
+@Data
+public class LoginForm {
+    String username;
+    String password;
+}
