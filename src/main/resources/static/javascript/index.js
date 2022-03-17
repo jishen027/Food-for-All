e91fd92a9d2e80acda45c5ff6a3a7ab0473be233
JipuLi <jishen027@gmail.com>
2022-03-17 17:30:53 +0000

    [MODIFY] index login function

---
+++ src/main/resources/static/javascript/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: Jipu Li 
+ * @Date: 2022-03-17 15:36:48 
+ * @Last Modified by: Jipu Li
+ * @Last Modified time: 2022-03-17 17:27:39
+ */
+
+var app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      title: 'Food for ALL',
+      loginState: false,
+    }
+  },
+  methods: {
+  },
+
+})
