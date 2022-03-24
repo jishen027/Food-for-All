982df4a4ed5fcb907e997bd183c6a0cefb916a5c
michaelvlh1 <lhvong1@sheffield.ac.uk>
2022-03-24 17:04:38 +0000

    Implemented paypal REST api and set up the framework for creating payment requests, currently gets the information needed from a form

--- src/main/java/com/team12/foodforall/controller/DonateController.java
+++ src/main/java/com/team12/foodforall/controller/DonateController.java
@@ -1,7 +1,18 @@
 package com.team12.foodforall.controller;
 
+import com.team12.foodforall.paypal.Order;
+import com.team12.foodforall.paypal.CreatePayment;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
 
 /**
  * @author: Long Hin Vong
@@ -10,8 +21,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DonateController {
 
-    @RequestMapping("/donate")
-    public String donate(){
+    @Autowired
+    CreatePayment service;
+
+    public static final String SUCCESS_URL = "pay/success";
+    public static final String CANCEL_URL = "pay/cancel";
+
+    @GetMapping("/donate")
+    public String donate() {
         return "donate";
     }
+
+    @PostMapping("/pay")
+    public String payment(@ModelAttribute("order") Order order) {
+        try {
+            Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
+                    order.getIntent(), order.getDescription(), "http://localhost:8000/" + CANCEL_URL,
+                    "http://localhost:8000/" + SUCCESS_URL);
+            for(Links link:payment.getLinks()) {
+                if(link.getRel().equals("approval_url")) {
+                    return "redirect:"+link.getHref();
+                }
+            }
+
+        } catch (PayPalRESTException e) {
+
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping(value = CANCEL_URL)
+    public String cancelPay() {
+        return "index";
+    }
+
+    @GetMapping(value = SUCCESS_URL)
+    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+        try {
+            Payment payment = service.executePayment(paymentId, payerId);
+            System.out.println(payment.toJSON());
+            if (payment.getState().equals("approved")) {
+                return "success";
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println(e.getMessage());
+        }
+        return "redirect:/";
+    }
+
+
 }
