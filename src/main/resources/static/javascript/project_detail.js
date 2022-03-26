c636ab01a409da28a4859c15510d9e69f71f2e8e
JipuLi <jishen027@gmail.com>
2022-03-26 19:43:43 +0000

    [UPDATE] new feature on project detail view

--- src/main/resources/static/javascript/project_detail.js
+++ src/main/resources/static/javascript/project_detail.js
@@ -2,7 +2,7 @@
  * @Author: Jipu Li 
  * @Date: 2022-03-20 21:28:28 
  * @Last Modified by: Jipu Li
- * @Last Modified time: 2022-03-26 19:00:00
+ * @Last Modified time: 2022-03-26 19:31:54
  */
 
 var app = new Vue({
@@ -11,6 +11,16 @@ var app = new Vue({
     return {
       title: 'Food For All',
       loginState: false,
+      project: {
+        title: 'Donate food for UK people',
+        content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea repellat aliquam minima iure quo sequi fuga ex assumenda maiores nemo illo dolor repudiandae alias unde saepe obcaecati possimus, nulla consequatur?',
+        img: '',
+        progress: 60,
+        positionName: 'UK',
+        positionLatLng: '',
+        charity: 'UK-Charity',
+        price: 8.99
+      }
     }
   },
   methods: {
