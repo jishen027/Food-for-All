b258b41aa0b8d07b68fecd690947f1641475a2c0
Gao Heng <hgao26@sheffield.ac.uk>
2022-03-29 16:18:18 +0100

    [ADD] add project entity

--- src/main/java/com/team12/foodforall/domain/Project.java
+++ src/main/java/com/team12/foodforall/domain/Project.java
@@ -2,9 +2,7 @@ package com.team12.foodforall.domain;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * @author: Heng Gao
@@ -14,8 +12,34 @@ import javax.persistence.Id;
 @Entity
 public class Project {
     @Id
-    @Column(name = "id", nullable = false)
+    @Column(name = "project_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "title", nullable = false)
     String title;
+
+    @Column(name = "content", nullable = false)
+    String content;
+
+    @Column(name = "img", nullable = false)
+    String img;  // '/Foodforall.jpeg',
+
+
+    String progress; //60,
+
+    String positionName; // 'UK',
+
+    Float Lat;
+
+    Float Lng; // <12.22, 23.55>
+
+    String charity; // 'UK-Charity',
+
+    String price; //'8.99',
+
+    String currency; //"$"
+
+    Long userId; // L
+
 }
