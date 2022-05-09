2dd3a2309f5667936d981100e52b2f4aad3e40b1
JipuLi <jishen027@gmail.com>
2022-05-09 18:26:08 +0100

    [MODIFY] index page projects

--- src/main/resources/static/javascript/index.js
+++ src/main/resources/static/javascript/index.js
@@ -2,7 +2,7 @@
  * @Author: Jipu Li 
  * @Date: 2022-03-17 15:36:48 
  * @Last Modified by: Jipu Li
- * @Last Modified time: 2022-05-09 17:20:29
+ * @Last Modified time: 2022-05-09 18:23:46
  */
 console.log(session)
 
@@ -11,41 +11,9 @@ var app = new Vue({
     data() {
         return {
             title: 'Food For All',
-            loginState: session.SPRING_SECURITY_CONTEXT.authentication.authenticated,
+            loginState: session.SPRING_SECURITY_CONTEXT.authentication.authenticated ? true : false,
             projects: [
-                {
-                    id: 1,
-                    title: 'Donate food for UK people',
-                    content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea repellat aliquam minima iure quo sequi fuga ex assumenda maiores nemo illo dolor repudiandae alias unde saepe obcaecati possimus, nulla consequatur?',
-                    img: '/Foodforall.jpeg',
-                    progress: 60,
-                    positionName: 'UK',
-                    positionLatLng: '',
-                    charity: 'UK-Charity',
-                    price: 8.99,
-                },
-                { 
-                    id: 2,
-                    title: 'Donate food for Asian',
-                    content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea repellat aliquam minima iure quo sequi fuga ex assumenda maiores nemo illo dolor repudiandae alias unde saepe obcaecati possimus, nulla consequatur?',
-                    img: '/FoodforAll02.jpeg',
-                    progress: 20,
-                    positionName: 'Asia',
-                    positionLatLng: '',
-                    charity: 'Asia-Charity',
-                    price: 7.99,
-                },
-                {
-                    id: 3,
-                    title: 'Donate food',
-                    content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea repellat aliquam minima iure quo sequi fuga ex assumenda maiores nemo illo dolor repudiandae alias unde saepe obcaecati possimus, nulla consequatur?',
-                    img: '/FoodforAll03.jpeg',
-                    progress: 30,
-                    positionName: 'UK',
-                    positionLatLng: '',
-                    charity: 'UK-Charity',
-                    price: 6.00,
-                }
+                projects[0], projects[1], projects[2]
             ]
         }
     },
--- src/main/resources/static/javascript/projects.js
+++ src/main/resources/static/javascript/projects.js
@@ -2,7 +2,7 @@
  * @Author: Jipu Li 
  * @Date: 2022-03-20 21:32:20 
  * @Last Modified by: Jipu Li
- * @Last Modified time: 2022-05-07 17:16:26
+ * @Last Modified time: 2022-05-09 17:43:18
  */
 
 console.log(session)
@@ -12,7 +12,7 @@ var app = new Vue({
     data() {
         return {
             title: 'Food For All',
-            loginState: session.SPRING_SECURITY_CONTEXT.authentication.authenticated,
+            loginState: session.SPRING_SECURITY_CONTEXT.authentication.authenticated? true:false,
             term: '',
             projects: projects,
             filteredProjects: projects,
